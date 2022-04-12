aa71439f988287667bec011edd3dc8fb3da6014e
Samuel Sherrer <samuel-sherrer@hotmail.com>
2022-04-12 08:28:11 -0300

    Update wsl-backup.ps1

--- wsl-backup.ps1
+++ wsl-backup.ps1
@@ -1,13 +1,24 @@
 Write-Host "Starting backup process at $(Get-Date)"
 
-$baseDir = "$HOME\wsl-backup\"
+$baseBackupDir = "$HOME\wsl-backup"
 $today = Get-Date -Format "yyyyMMdd"
-$backupFile = "$baseDir\ubuntu-backup-$today.tar"
+$backupFile = "$baseBackupDir\ubuntu-backup-$today.tar"
+
 wsl --export Ubuntu-20.04 $backupFile
 
 Write-Host "Backup completed at $(Get-Date)"
 
-Write-Host "Removing $(Get-ChildItem $baseDir | Sort-Object -Descending -Property LastWriteTime | select -Skip 2)"
-Get-ChildItem $baseDir | Sort-Object -Descending -Property LastWriteTime | select -Skip 2 | Remove-Item -Force
+[string[]]$filesToRemove = Get-ChildItem $baseBackupDir | Sort-Object -Descending -Property LastWriteTime | select -Skip 2
+
+if ( $filesToRemove.Count -gt 0 )
+{
+    # Keep only the two most recent backups.
+	Write-Host "Removing files."
+	Get-ChildItem $baseBackupDir | Sort-Object -Descending -Property LastWriteTime | select -Skip 2 | Remove-Item
+}
+else
+{
+	Write-Host "There is no file to remove."
+}
 
 Write-Host "Done at $(Get-Date)"
